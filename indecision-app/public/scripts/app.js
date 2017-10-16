ea25ded07ad7e39ed2cddb5ec4aaabcad9d37959
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-10-17 00:39:37 +0300

    Babel setup

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,14 @@
-console.log('app.js is running!')
+'use strict';
+
+console.log('app.js is running!');
 
 // JSX - Javascript XML
-//var template = <h1 id="someid">This is JSX from app.js!</h1>;
-
 var template = React.createElement(
-    'h1',
-    { id: "someid" },
-    'This is JSX from app.js'
-)
-var appRoot = document.getElementById('app')
+  'h1',
+  null,
+  ' Indecision app!'
+);
+
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log('app.js is running!')
+
+// JSX - Javascript XML
+var template = <h1> Indecision app!</h1>;
+
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot);
