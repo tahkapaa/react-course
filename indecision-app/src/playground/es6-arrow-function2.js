d7df3be432fca607e5cd780110ae54e8bf2a44b8
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-10-24 22:44:44 +0300

    add arrow function challenge

--- indecision-app/src/playground/es6-arrow-function2.js
+++ indecision-app/src/playground/es6-arrow-function2.js
@@ -1,25 +1,19 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
     // console.log(arguments);
-    return a + b;
-};
+    return a+b;
+}
 
 console.log(add(55, 1));
 
 // this keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Pasi',
     cities: ['Tampere', 'Lempäälä', 'Orimattila'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city);
 
         // this.cities.forEach((city) => {
         //     console.log(this.name + ' has lived in ' + city);
@@ -31,18 +25,14 @@ console.log(user.printPlacesLived());
 
 // Challenge are
 
-var multiplier = {
+const multiplier = {
     // numbers - array of number
     // multilyBy - single number
     // multiply - return a new array where the number have been multiplied
     numbers: [1, 2, 3, 4, 5],
     multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (n) {
-            return n * _this2.multiplyBy;
-        });
+    multiply() {
+        return this.numbers.map((n) => n*this.multiplyBy);
     }
 };
 
