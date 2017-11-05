7b536de70c5b85b04a908a4f4a811d20bdf5917e
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-11-05 21:35:24 +0200

    Add es6 subclass challenge

--- indecision-app/src/playground/es6-classes.js
+++ indecision-app/src/playground/es6-classes.js
@@ -14,6 +14,54 @@ class Person {
     }
 }
 
-const me = new Person('Pasi', 42);
-console.log(me.getGreeting());
+class Student extends Person {
+    constructor(name, age, major = 'Undecided') {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return this.major != 'Undecided';
+    }
+
+    getDescription() {
+        let description = super.getDescription()
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+        return description;
+    }
+}
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (!!this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
+
+const me = new Student('Pasi', 42, 'Computer Science');
+console.log(me);
 console.log(me.getDescription());
+console.log(me.getGreeting());
+
+const other = new Person();
+console.log(other);
+console.log(other.getDescription());
+console.log(other.getGreeting());
+
+const luckyOne = new Traveller('Laura', '40', 'Helsinki');
+console.log(luckyOne);
+console.log(luckyOne.getDescription());
+console.log(luckyOne.getGreeting());
