a20deb488a96cb4d53cd5073ffb06b5c1d19ad89
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-11-08 20:18:52 +0200

    export default example

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,14 @@
 //import './utils';
-// import { add, square } from './utils';
+// import subtract, { add, square } from './utils';
 
 // console.log('app.js running');
 
 // console.log(square(4));
 // console.log(add(3, 4));
+// console.log(subtract(3, 4));
 
-import { isAdult, canDrink } from './person';
+import isSenior, { isAdult, canDrink } from './person';
 
 console.log(isAdult(18));
 console.log(canDrink(17));
+console.log(isSenior(65));
--- indecision-app/src/person.js
+++ indecision-app/src/person.js
@@ -1,3 +1,7 @@
 export const isAdult = (age) => age >= 18 ? true : false;
 
 export const canDrink = (age) => isAdult(age);
+
+const isSenior = (age) => age >= 65 ? true : false;
+
+export default isSenior;
