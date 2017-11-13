3efe98e3ba466ab6b0b3543201b1c3dbfa727e70
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-11-13 19:46:48 +0200

    Style the buttons

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
     <div>
         <li>{props.optionText}</li>
         <button
+            className="button button--link"
             onClick={(e) => {
             props.handleDeleteOption(props.optionText);
         }}>
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -3,7 +3,7 @@ import Option from './Option'
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>
+        <button className="button button--link" onClick={props.handleDeleteOptions}>
             Remove all
         </button>
         {props.options.length === 0 && <p>Please add an option to get started!</p>}
