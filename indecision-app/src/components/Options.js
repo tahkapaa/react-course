410e8dde4c5267ec6f8dba2ed21411107d370680
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-11-13 20:27:30 +0200

    Styling Add Option

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -12,9 +12,10 @@ const Options = (props) => (
         {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
         {props
             .options
-            .map((option) => (<Option
+            .map((option, index) => (<Option
                 key={option}
                 optionText={option}
+                count={index + 1}
                 handleDeleteOption={props.handleDeleteOption}/>))}
     </div>
 );
