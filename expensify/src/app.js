2d7f079eb801582a21efd768aa00601c27e83ddb
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-11-15 21:43:35 +0200

    Add links to routes

--- expensify/src/app.js
+++ expensify/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,19 +30,35 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404 -
+        <Link to="/">Go home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/create">Create</Link>
+            <Link to="/edit">Edit</Link>
+            <Link to="/help">Help</Link>
+        </nav>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDasboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path="/" component={ExpenseDasboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
